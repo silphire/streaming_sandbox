dfb3882af6e06693be907944405d0d2188b07c79
Yuki Mitsui <silphire@gmail.com>
2019-01-23 00:00:49 +0900

    implemented make_cmaf.sh

--- cmaf_sample/make_cmaf.sh
+++ cmaf_sample/make_cmaf.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+BENTO4_ROOT=
+FFMPEG_ROOT=
+
+MEDIA_FILE=$1
+
+# create fragmented MP4
+$BENTO4_ROOT/mp4fragment $MEDIA_FILE fragmented_$MEDIA_FILE
+
+# create manifest playlist
+$BENTO4_ROOT/mp4dash fragmented_$MEDIA_FILE
+
 exit
 
