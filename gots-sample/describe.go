c2358e2ed8f8edcebe83127b155a1cf53817a749
Yuki Mitsui <silphire@gmail.com>
2019-02-03 23:27:00 +0900

    implemented file open

--- gots-sample/describe.go
+++ gots-sample/describe.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Comcast/gots/packet"
 )
 
 func main() {
 	var pkt = packet.Packet
-	fmt.Printf("")
+
+	file, err = os.Open("test.mp4")
+	if err != null {
+		fmt.Printf("Cannot open file -- %s", err.Error())
+		os.Exit()
+	}
+
 }
