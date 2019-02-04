03bc9ce42283906e569627d8c1c12ba3106e1593
Yuki Mitsui <silphire@gmail.com>
2019-02-04 22:11:09 +0900

    make it compilable

--- gots-sample/describe.go
+++ gots-sample/describe.go
@@ -8,12 +8,33 @@ import (
 )
 
 func main() {
-	var pkt = packet.Packet
-
-	file, err = os.Open("test.mp4")
-	if err != null {
+	file, err := os.Open("test.mp4")
+	if err != nil {
 		fmt.Printf("Cannot open file -- %s", err.Error())
-		os.Exit()
+		os.Exit(1)
 	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Printf("cannot close file\n")
+			os.Exit(1)
+		}
+	}(file)
 
+	var pkt packet.Packet
+	for {
+		readLen, err := file.Read(pkt[:])
+		if readLen <= 0 {
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("packet read error")
+			os.Exit(1)
+		}
+
+		pid := packet.Pid(&pkt)
+
+		fmt.Printf("read packet identifier: %d\n", pid)
+	}
 }
